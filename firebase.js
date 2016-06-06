87a6cfdbfc89c2b8b2e1702825708cc84859ef1f
Armaan Shah <armaanshah96@gmail.com>
2016-06-06 15:57:26 -0500

    commiting firebase changes for push

--- firebase.js
+++ firebase.js
@@ -3,29 +3,53 @@
 // be able to count the number of people in a group
 // be able to retrieve the users in a specific group
 // listen for new users
+'use strict';
 
-var ref = new Firebase("https://project-6451685085117135161.firebaseio.com/");
-var usersRef = ref.child("users");
-var groupRef = ref.child("groups");
-ref.initializeGroups();
+var ref;
+var usersRef;
+var groupRef;
+var timeChosen = "";
+var dateChosen = "";
+var airportChosen = "";
+var usernameChosen = "";
+var emailChosen = "";
+var groupChosen = "";
+var phoneChosen = "";
+
+window.onload = function() {
+  ref = new Firebase("https://project-6451685085117135161.firebaseio.com/");
+  usersRef = ref.child("users");
+  groupRef = ref.child("groups");
+  initializeGroups();
+  timeChosen = "";
+  dateChosen = "";
+  airportChosen = "";
+  usernameChosen = "";
+  emailChosen = "";
+  phoneChosen = "";
+  groupChosen = "";
+}
 
 function initializeGroups() {
-  date_array = [7,8,9,10,11,12,13]
-  group_ids = []
-  for (airports=0; airports< 2; airports++)  
-    for(dates = 0; dates < 7; dates++)
-      for(times =1; times < 25; times++ )
+  var date_array = [7,8,9,10,11,12,13]
+  var group_ids = []
+  for (var airports=0; airports< 2; airports++)  
+    for(var dates = 0; dates < 7; dates++)
+      for(var times =1; times < 25; times++ )
+      {
         if(airports === 0)
-          airport = "Ohare"
+          var airport_name = "Ohare"
         else
-          airport = "Midway"
-        input = {airport: "Ohare", date: dates_array[dates], time: times}
+          var airport_name = "Midway"
+        var date = date_array[dates]
+        var input = {airport: airport_name, date: date, time: times}
         //members child is to be inputted when updategroup is called for the first time
-
-        group_ids.push(groupRef.push(input).key()) //keep track of all the groups to access later
-
+        groupRef.push(input)
+      }
+        // group_ids.push(groupRef.push(input).key()) //keep track of all the groups to access later
+  return true;
 	/*
-		set the group_id's for each location,date,time combination
+		set the group_id's for each location,date,time combination -- most likely using nested for loops for each date-times for ohare and midway
 		define :
 			group_id {
 				'airport': 'Ohare',
@@ -40,152 +64,117 @@ function initializeGroups() {
 // all parameters are given when function is called, most importantly the userGroup_Id is provided based on a match
 // with the group_id that represents the button pressed
 function writeNewUser(username, userEmail, userPhone, group_id) {
-	input = {name: username, email: userEmail, phone: userPhone, group_id: true}
-  var user_id = usersRef.push(input).key;
+  console.log("creating user!")
+	input = {name: username, email: userEmail, phone: userPhone, [group_id]: true}
+  console.log("user looks like this:" + input)
+  var user_id = usersRef.push(input).key();
+
+  console.log("user id has been pushed! the user_id is: " + user_id + " now lets go update groups!")
 	ref.updateGroup(user_id,group_id)
 }
 
 function updateGroup(userId, userGroup_Id) {
 	groupRef.child(userGroup_Id).child('members').update({
-		userId:true
+		[userId]:true
 	});
 	// ENSURE THAT UPDATE DOES NOT REPLACE THE EXISTING MEMBERS AND ONLY APPENDS ONE MORE ITEM TO THE END
 }
 
 function getGroupMembers(group_id) {
 	var members_list = []
-	groupRef.child(group_id).child('members').forEach(function(childSnapshot) {
-		members_list.push(childrenSnapshot.key())
-	});
+	groupRef.child(group_id).child('members').once("value", function(snapshot) { 
+    snapshot.forEach(function(childSnapshot) {
+		  members_list.push(childrenSnapshot.key())
+	 });
+  });
 }
 
 
+function airport(airport_id) {
+  airportChosen = airport_id;
+  console.log(airportChosen);
+}
 
+function date(date_id) {
+  dateChosen = date_id;
+  console.log(dateChosen);
+}
 
-function isUserInGroup(userId) {
-    ref.once('value', function(snap) {
-    var result = snap.val() === null? return true : return false;
+function time(time_id) {
+  timeChosen = time_id;
+  console.log(timeChosen);
+}
+
+function updateName(name_id) {
+  usernameChosen = document.getElementById("username").value;
+  console.log(usernameChosen);
+}
+
+function updateEmail(email_id) {
+  emailChosen = document.getElementById("useremail").value;
+  console.log(emailChosen);
+}
+
+// function updatePhone(phone_id) {
+//   phoneChosen = phone_id;
+// }
+
+function findGroup() {
+  groupRef.once("value",function(snap) {
+    snap.forEach(function(snapshot) {
+      var data = snapshot.val();
+      if (data.airport === airportChosen) {
+        if (data.date === dateChosen) {
+          if(data.time === timeChosen) {
+            console.log("snapshot/group_id is ");
+            console.log(snapshot.key());
+            groupChosen = snapshot.key();
+            return;
+          }
+        }
+      }
     });
-}
-
-
-/**
- * Starts listening for new posts and populates posts lists.
- */
-function startDatabaseQueries() {
-  // [START my_top_posts_query]
-  var myUserId = firebase.auth().currentUser.uid;
-  var topUserPostsRef = firebase.database().ref('user-posts/' + myUserId).orderByChild('starCount');
-  // [END my_top_posts_query]
-  // [START recent_posts_query]
-  var recentPostsRef = firebase.database().ref('posts').limitToLast(100);
-  // [END recent_posts_query]
-  var userPostsRef = firebase.database().ref('user-posts/' + myUserId);
-
-  var fetchPosts = function(postsRef, sectionElement) {
-    postsRef.on('child_added', function(data) {
-      var containerElement = sectionElement.getElementsByClassName('posts-container')[0];
-      containerElement.insertBefore(
-          createPostElement(data.key, data.val().title, data.val().body, data.val().author),
-          containerElement.firstChild);
-    });
-  };
-
-  fetchPosts(topUserPostsRef, topUserPostsSection);
-  fetchPosts(recentPostsRef, recentPostsSection);
-  fetchPosts(userPostsRef, userPostsSection);
-}
-
-/**
- * Writes the user's data to the database.
- */
-// [START basic_write]
-function writeUserData(userId, name, email) {
-  firebase.database().ref('users/' + userId).set({
-    username: name,
-    email: email
   });
+  // return false;
 }
-// [END basic_write]
 
-// Bindings on load.
-window.addEventListener('load', function() {
-  // Bind Sign in button.
-  signInButton.addEventListener('click', function() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  });
-
-  // Listen for auth state changes
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      splashPage.style.display = 'none';
-      writeUserData(user.uid, user.displayName, user.email);
-      startDatabaseQueries();
-    } else {
-      splashPage.style.display = 'block';
+function finishUser() {
+  updateName();
+  updateEmail();
+  findGroup();
+  console.log(groupChosen);
+  console.log(usernameChosen);
+  console.log(emailChosen);
+  if (groupChosen !== false && usernameChosen !== null && emailChosen !== null && emailChosen.includes("northwestern.edu") === true) {
+    console.log("did it pass?")
+    writeNewUser(usernameChosen, emailChosen, phoneChosen, groupChosen)
+    document.getElementById("finish").innerHTML = "That's it! We'll be in touch !";
+  }
+  else {
+    //ERROR CHECKING
+    if (usernameChosen === null) {
+      document.getElementById("name_input").innerHTML = "Full Name REQUIRED";
     }
-  });
-
-  // Saves message on form submit.
-  messageForm.onsubmit = function(e) {
-    e.preventDefault();
-    if (messageInput.value && titleInput.value) {
-      var postText = messageInput.value;
-      messageInput.value = '';
-      // [START single_value_read]
-      var userId = firebase.auth().currentUser.uid;
-      firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-        var username = snapshot.val().username;
-        // [START_EXCLUDE]
-        writeNewPost(firebase.auth().currentUser.uid, firebase.auth().currentUser.displayName,
-            titleInput.value, postText).then(function() {
-              myPostsMenuButton.click();
-            });
-        // [END_EXCLUDE]
-      });
-      // [END single_value_read]
+    if (emailChosen === null) {
+      document.getElementById("email_input").innerHTML = "NU Email REQUIRED";
     }
-  };
+    if (emailChosen.includes("northwestern.edu") === false) {
+      document.getElementById("email_input").innerHTML = "Invalid NU Email";
+    }
+    if(groupChosen === false) {
+      if(airportChosen === null) {
+        document.getElementById("button_problem").innerHTML = "Make sure to choose your airport!";
+      }
+      if(dateChosen === null) {
+        document.getElementById("button_problem").innerHTML = "Make sure to choose your date!";
+      }
+      if(timeChosen === null) {
+        document.getElementById("button_problem").innerHTML = "Make sure to choose your time!";
+      }
+    }
+    return false;
+  }
+  return true;
+}
 
-  // Bind menu buttons.
-  recentMenuButton.onclick = function() {
-    recentPostsSection.style.display = 'block';
-    userPostsSection.style.display = 'none';
-    topUserPostsSection.style.display = 'none';
-    addPost.style.display = 'none';
-    recentMenuButton.classList.add('is-active');
-    myPostsMenuButton.classList.remove('is-active');
-    myTopPostsMenuButton.classList.remove('is-active');
-  };
-  myPostsMenuButton.onclick = function() {
-    recentPostsSection.style.display = 'none';
-    userPostsSection.style.display = 'block';
-    topUserPostsSection.style.display = 'none';
-    addPost.style.display = 'none';
-    recentMenuButton.classList.remove('is-active');
-    myPostsMenuButton.classList.add('is-active');
-    myTopPostsMenuButton.classList.remove('is-active');
-  };
-  myTopPostsMenuButton.onclick = function() {
-    recentPostsSection.style.display = 'none';
-    userPostsSection.style.display = 'none';
-    topUserPostsSection.style.display = 'block';
-    addPost.style.display = 'none';
-    recentMenuButton.classList.remove('is-active');
-    myPostsMenuButton.classList.remove('is-active');
-    myTopPostsMenuButton.classList.add('is-active');
-  };
-  addButton.onclick = function() {
-    recentPostsSection.style.display = 'none';
-    userPostsSection.style.display = 'none';
-    topUserPostsSection.style.display = 'none';
-    addPost.style.display = 'block';
-    recentMenuButton.classList.remove('is-active');
-    myPostsMenuButton.classList.remove('is-active');
-    myTopPostsMenuButton.classList.remove('is-active');
-    messageInput.value = '';
-    titleInput.value = '';
-  };
-  recentMenuButton.onclick();
-}, false);
+
